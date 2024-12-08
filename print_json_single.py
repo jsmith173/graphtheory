675596c81501e83b7968c9d8e2fb3f1768dc261b
attx <attx@localhost>
2024-12-08 15:01:56 +0100

    a

--- print_json_single.py
+++ print_json_single.py
@@ -13,14 +13,17 @@ if not os.path.isdir('temp'):
 if not os.path.isdir('indent'):
 	os.mkdir('indent')
 
-fn = "temp.json"	
-fn_ = f"data/{fn}"
-fn2 = Path(fn_)
-fn_wo_ext = str(fn2.with_suffix(''))
-fn2 = Path(fn_wo_ext)
-fn2_wo_ext = fn2.name
+with open("data/filelist.json", 'r') as f:
+	file_list = json.load(f)
 
-with open(fn_, 'r') as f:
-	json_data = json.load(f)
+for fn in file_list['filelist']:
+	fn_ = f"data/{fn}"
+	fn2 = Path(fn_)
+	fn_wo_ext = str(fn2.with_suffix(''))
+	fn2 = Path(fn_wo_ext)
+	fn2_wo_ext = fn2.name
 
-dump_list(json_data, 'indent/'+fn2_wo_ext+'-indent.json')
+	with open(fn_, 'r') as f:
+		json_data = json.load(f)
+
+	dump_list(json_data, 'indent/'+fn2_wo_ext+'-indent.json')
